38ea059a6ce8e2dcd8b6640912c8e5df5b397d43
DLorell <d.lorell@yahoo.com>
2020-06-26 02:31:35 -0500

    deletere

--- deleter.py
+++ deleter.py
@@ -6,10 +6,11 @@ import sys
 model_dirs = [d for d in os.listdir("models") if os.path.isdir("models/"+d)]
 for model_dir in model_dirs:
     saves = [f for f in os.listdir("models/"+model_dir) if f.endswith(".pt")]
-    print(saves)
     if "200.pt" in saves:
         for save in saves:
             os.remove("models/"+model_dir+"/"+save)
+            with open("models/"+model_dir+"/"+save, 'w') as fp: 
+                pass
 
 
 
